ad966a5e9208001c109db8c40385fb6532f4d0e5
legion <bagulshubham72@gmail.com>
2025-06-21 16:53:04 +0530

    Added image uplaod and cloudinary support

--- controller/PostController.js
+++ controller/PostController.js
@@ -1,4 +1,5 @@
 const Post = require("../modules/post");
+const {uploadImageToCloudinary} = require("../utils/imageUploader")
 
 exports.createPost = async(req,res) => {
     try{
@@ -6,14 +7,24 @@ exports.createPost = async(req,res) => {
         const {discription, media, postType} = req.body;
         const userId = req.userId;
 
+        const file = req.files.media;
+
         if(!discription || !postType) {
             return res.status(400).json({
                 success: false,
                 message: "Discription and postType are required"
             })
         }
+        
+        const image = await uploadImageToCloudinary(
+            file,
+            process.env.FOLDER_NAME,
+            1000,
+            1000
+        )
+        const imageUrl = image.secure_url;
 
-        const createdPost = await Post.create({discription, media, postType, userId});
+        const createdPost = await Post.create({discription, media  : imageUrl, postType, userId});
         
         return res.status(200).json({
             success: true,
